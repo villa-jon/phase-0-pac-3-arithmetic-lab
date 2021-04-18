a778b34003eed86e57d979a47d8d1c9d8ac8542d
Jonathan Villanueva <villanuevajonathan606@gmail.com>
2021-04-18 04:01:38 -0700

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,13 @@
+function add(number1,number2) {return number1+number2;}
+function subtract(number2,number1) {return number2-number1;}
+function multiply(number1,number2) {return number1*number2;}
+function divide(number2,number1) {return number2/number1;}
+function increment (n) {return n+=1}
+function decrement (n) {return n-=1}
+function makeInt (n)
+    {return parseInt(n, 10);
+    }
+function preserveDecimal(n)
+        {return parseFloat(n,10);
+           
+        }
